a060d96487e06564e02334b36704d91e294277c1
Papaya <stephaniesosa02@gmail.com>
2025-02-04 22:19:08 -0700

    starter codes

---
+++ server/src/server.ts
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
